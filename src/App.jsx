bcaa6888e4ab967b4bbaf8ebc6da2aa6a4120918
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-28 18:23:14 -0600

    inital commit 💻

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <article className="App">
+      <di>
+        <h1>Favorite Contacts</h1>
+      </di>
+      <div>
+        <div>
+          <h3>Name</h3>
+        </div>
+        <div>
+          <h3>Lastname</h3>
+        </div>
+        <div>
+          <h3>Last call</h3>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default App;
