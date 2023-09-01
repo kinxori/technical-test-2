6953932db17504665a95d88a3c36c3c28b2faa03
kinxori <116584982+kinxori@users.noreply.github.com>
2023-09-01 17:40:41 -0600

    adding phone number column

--- src/components/UserFilter.jsx
+++ src/components/UserFilter.jsx
@@ -35,9 +35,9 @@ export default function UserFilterComponent() {
       <div className="bg-purple-600  py-3 flex justify-center items-center  ">
         <h1 className="text-[24px] text-white font-bold ">Favorite Contacts</h1>
       </div>
-      <div className="border-box p-5 overflow-auto grid grid-flow-row">
+      <div className="border-box p-5 overflow-auto">
         <table className="w-full border-gray-400 border-[1px] text-black border-collapse ">
-          <tr className="text-center bg-black/20">
+          <tr className="text-center bg-black/20 ">
             <th className=" border-gray-400 border-[1px]">
               <i>Name</i>
             </th>
@@ -47,22 +47,26 @@ export default function UserFilterComponent() {
             <th className=" border-gray-400 border-[1px] ">
               <i>mm/dd/yyyy</i>
             </th>
+            <th className=" border-gray-400 border-[1px] ">
+              <i>Phone #</i>
+            </th>
           </tr>
           {filteredData.map((item) => (
-            <tr key={item.phoneNumber + item.called} className="text-center">
+            <tr key={item.phoneNumber + item.called} className="text-center ">
               <td className="border-gray-400 border-[1px] ">
                 <h3>
                   {item.firstName} <span className="font-bold">{item.lastName}</span>
                 </h3>
               </td>
               <td className="border-gray-400 border-[1px] ">{item.numberOfCalls}</td>
-              <td className="border-gray-400 border-[1px]">
+              <td className="border-gray-400 border-[1px] ">
                 {new Date(item.called * 1000).toLocaleDateString("en-US", {
                   year: "numeric",
                   month: "numeric",
                   day: "numeric",
                 })}
               </td>
+              <td className="border-gray-400 border-[1px]">{item.phoneNumber}</td>
             </tr>
           ))}
         </table>
