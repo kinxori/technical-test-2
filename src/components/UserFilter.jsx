51b92b5ee39929e761b02bbcf1b5c51a8a77d9b6
kinxori <116584982+kinxori@users.noreply.github.com>
2023-09-01 22:00:56 -0600

    updated tablet structure - removed <favorite contacts> title

--- src/components/UserFilter.jsx
+++ src/components/UserFilter.jsx
@@ -32,44 +32,45 @@ export default function UserFilterComponent() {
 
   return (
     <article className="rounded-md w-full flex flex-col justify-start border-white overflow-hidden bg-white  ">
-      <div className="bg-purple-600  py-3 flex justify-center items-center  ">
-        <h1 className="text-[24px] text-white font-bold ">Favorite Contacts</h1>
-      </div>
-      <div className="border-box p-5 overflow-auto">
-        <table className="w-full border-gray-400 border-[1px] text-black border-collapse ">
-          <thead>
-            <tr className="text-center bg-black/20 h-[50px]">
-              <th className=" border-gray-400 border-[1px] min-w-[150px]   ">
+      <div className="border-box p-5 overflow-auto ">
+        <table className="w-full rounded-md overflow-hidden shadow-lg shadow-black/20 border-gray-400 border-y-[1px] text-black border-collapse ">
+          <thead className="text-center bg-purple-600 h-[70px] text-white ">
+            <tr className=" border-gray-400 border-y-[1px]  ">
+              <th className="min-w-[150px]   ">
                 <i>Name</i>
               </th>
-              <th className=" border-gray-400 border-[1px] min-w-[100px]">
+              <th className="min-w-[100px]">
                 <i># of Calls</i>
               </th>
-              <th className=" border-gray-400 border-[1px] min-w-[150px]">
-                <i>mm/dd/yyyy</i>
+              <th className="min-w-[150px]">
+                <i>Last Call </i>
+                <h5 className="text-white/50 text-[10px] font-normal ">(mm/dd/yyyy)</h5>
               </th>
-              <th className=" border-gray-400 border-[1px] min-w-[150px]  ">
-                <i>Phone #</i>
+              <th className="min-w-[150px]  ">
+                <i>Phone Number</i>
               </th>
             </tr>
           </thead>
           {filteredData.map((item) => (
-            <tbody key={item.phoneNumber + item.called}>
-              <tr className="text-center  h-[35px] even:bg-blue-100">
-                <td className="border-gray-400 border-[1px] ">
+            <tbody
+              key={item.phoneNumber + item.called}
+              className="even:bg-purple-50 h-[50px] text-center "
+            >
+              <tr className="border-gray-400 border-y-[1px]">
+                <td>
                   <h3>
                     {item.firstName} <span className="font-bold">{item.lastName}</span>
                   </h3>
                 </td>
-                <td className="border-gray-400 border-[1px] ">{item.numberOfCalls}</td>
-                <td className="border-gray-400 border-[1px] ">
+                <td>{item.numberOfCalls}</td>
+                <td>
                   {new Date(item.called * 1000).toLocaleDateString("en-US", {
                     year: "numeric",
                     month: "numeric",
                     day: "numeric",
                   })}
                 </td>
-                <td className="border-gray-400 border-[1px]">{item.phoneNumber}</td>
+                <td>{item.phoneNumber}</td>
               </tr>
             </tbody>
           ))}
