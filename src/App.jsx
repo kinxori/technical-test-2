24497a20209c1456c44761db44d437da05f2c14b
kinxori <116584982+kinxori@users.noreply.github.com>
2023-09-01 18:08:54 -0600

    landing page done - starting body of project

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ export default function App() {
     <section className="relative ">
       <img src={bgImg} className="fixed z-[-1000] w-screen h-screen object-cover " />
       <div className="border-box p-10 flex flex-col gap-5">
-        <div className="flex flex-col gap-3">
-          <h1 className="font-[rubik] font-bold text-[60px] underline">Hey there!</h1>
-          <p className="text-justify">
+        <div className="flex flex-col gap-3 text-justify">
+          <h1 className="font-[rubik] font-bold text-[60px] text-left underline">Hey there!</h1>
+          <p>
             Welcome, this is my second #technicalTest I've had to complete for a Frontend JR role!
           </p>
           <p>
-            The main purpose of this project was to test <b className="italic">Array Methods</b> and
-            how to filter and sort data. For this example there was 3 main points to complete:
+            The main purpose of this project was to test <b className="italic">Array Methods</b>.
+            For this example there was 3 main points to complete:
           </p>
-          <ul className="list-disc pl-5">
+          <ul className="list-disc pl-8">
             <li>Remove repeated contacts</li>
             <li>Sorting contacts by number of calls</li>
-            <li>Sorting contacts by recent history</li>
+            <li>Sorting contacts by call history</li>
           </ul>
         </div>
         <UserFilter />
